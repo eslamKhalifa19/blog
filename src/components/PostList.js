ac8632f4d730357c87b5f155ce0edfb9b1c39d83
eslam <eslamkhalefa19@gmail.com>
2019-02-19 06:25:58 +0200

    make api request getting posts data

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions";
 
 class PostList extends Component {
-  state = {};
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
+    console.log(this.props.posts);
     return <div>Post List</div>;
   }
 }
 
-export default PostList;
+const mapStateToProps = state => {
+  return { posts: state.posts };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchPosts }
+)(PostList);
